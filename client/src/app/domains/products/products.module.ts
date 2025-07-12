09a5babe4bc40f0f13339ece4bea999891a00d00
ROGER MESTANZA <rogers.mestanza@gmail.com>
2025-07-12 11:16:29 -0500

    Add ProductsPageComponent to project done

---
+++ client/src/app/domains/products/products.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+
+@NgModule({
+  declarations: [
+    ProductsPageComponent
+  ],
+  imports: [],
+  providers: [],
+  exports: [],
+})
+export class ProductsModule {}
